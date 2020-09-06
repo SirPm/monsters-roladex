b15042d18fd5ba752a3c0a6a9d7573e9f3d115f0
SirPm <odumosuoluwapm@gmail.com>
2020-09-06 13:20:33 +0100

    Updates

---
+++ precache-manifest.d8ec7d8c459d139f1207b18b5a7d4aa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d23afa377edb3587782723f12dc63afc",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "4c3e30d8692ed53da7e3",
+    "url": "/monsters-roladex/static/css/main.a69a676f.chunk.css"
+  },
+  {
+    "revision": "ddfc98fd1fb7cc1baeab",
+    "url": "/monsters-roladex/static/js/2.fcb60162.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roladex/static/js/2.fcb60162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3e30d8692ed53da7e3",
+    "url": "/monsters-roladex/static/js/main.7e54eaa9.chunk.js"
+  },
+  {
+    "revision": "7ea191c2310c59cc6381",
+    "url": "/monsters-roladex/static/js/runtime-main.1b90d830.js"
+  }
+]);
